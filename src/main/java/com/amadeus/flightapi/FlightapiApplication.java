c6b488bf57af500f9319554554f0cb179348c417
Kerim Şentürk <kerimsenturk2002@outlook.com>
2024-02-02 21:49:47 +0100

    spring initialize...

---
+++ src/main/java/com/amadeus/flightapi/FlightapiApplication.java
@@ -0,0 +1,13 @@
+package com.amadeus.flightapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightapiApplication.class, args);
+	}
+
+}
